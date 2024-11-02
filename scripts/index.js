4d3a30682b68de183c91db7efb833bfb79160dee
Bas <anna.basulina@yandex.ru>
2024-11-02 13:49:03 +0300

    second

--- scripts/index.js
+++ scripts/index.js
@@ -2,23 +2,23 @@
 const template = document.querySelector('#card-template').content; 
 const cardsContainer = document.querySelector('.places__list');
 
-function addCard(name, link) {
+function createCard(name, link) {
     const copyTemplateElement = template.querySelector('.card').cloneNode(true);
 
     copyTemplateElement.querySelector('.card__image').src = link;
+    copyTemplateElement.querySelector('.card__image').alt = name;
     copyTemplateElement.querySelector('.card__title').textContent = name;
-    
+
     const deleteButton = copyTemplateElement.querySelector('.card__delete-button');
     deleteButton.addEventListener('click', function() {
         copyTemplateElement.remove(); 
-        /*const card = deleteButton.closest('.card');
-        card.remove();*/
+
     });
 
-    cardsContainer.append(copyTemplateElement); 
+    return copyTemplateElement;
 }
 
 initialCards.forEach(function(element) {
-    addCard(element.name, element.link);
-
+    const createdCard = createCard(element.name, element.link);
+    cardsContainer.append(createdCard);
 });
